202a7a2a93a34178c1a2004a25cbd226e191efc1
G4BR13L-hub <gabrielgodoynavarro0@gmail.com>
2025-01-16 19:03:52 +0000

    ImageDisplay created

---
+++ src/main/java/software/ulpgc/imageviewer/view/ImageDisplay.java
@@ -0,0 +1,26 @@
+package software.ulpgc.imageviewer.view;
+
+public interface ImageDisplay {
+    void drawImage(String id, int offset);
+    int getWidth();
+    void clear();
+    void onResize(SizeListener listener);
+    void on(Displacement shift);
+    void on(Releasement released);
+
+    interface SizeListener {
+        void resized(int width, int height);
+        SizeListener NULL = (width, height) -> {};
+    }
+
+    interface Displacement {
+        void offset(int offset);
+        Displacement Null = offset -> {};
+
+    }
+
+    interface Releasement {
+        void offset(int offset);
+        Releasement Null = offset -> {};
+    }
+}
